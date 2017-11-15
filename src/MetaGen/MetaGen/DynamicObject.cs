ce80d5e46057c50e9cab6c1f875f20925c692bd2
Alex Novak <alexwnovak@gmail.com>
2017-11-14 21:14:49 -0500

    WIP

---
+++ src/MetaGen/MetaGen/DynamicObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetaGen
+{
+  public abstract class DynamicObject
+  {
+
+    public void Create()
+    {
+      
+    }
+  }
+}
